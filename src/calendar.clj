eef1b1c47f428aa34376e5d81a65ead36b844b65
Jonathan Dahan <hi@jonathan.is>
2017-12-29 16:16:19 -0500

    Update to use new screen-width, text :right, and :highest render hints

--- src/calendar.clj
+++ src/calendar.clj
@@ -10,11 +10,8 @@
             [clj-time.predicates :as is]
             [metapng.core :as metapng]))
 
-(defn right-text
-  "Draw text right aligned"
-  [canvas ^String s ^long x ^long y]
-  (let [width (.stringWidth (.getFontMetrics (.graphics canvas)) s)]
-    (c/text canvas s (- x width) y)))
+(defn canvas-font-height [canvas]
+  (.getHeight (.getFontMetrics (.graphics canvas))))
 
 (defn day-to-grid [months-count longest-month day]
   "Maps a day to a normalized grid with 12 rows and 31+ columns"
@@ -42,23 +39,14 @@
   "s makes a snapshot image"
   (assoc state :snapshot true))
 
-(defmethod c/key-pressed [(str *ns*) c/virtual-key] [event state]
-  "q/esc for exit"
-  (let [k (c/key-code event)]
-    (cond
-      (= :esc k) (assoc state :close true)
-      (= :q k) (assoc state :close true)
-      :else (do
-              (println (str "unknown key " event))
-              state))))
+(defmethod c/key-pressed [(str *ns*) \q] [event state]
+  "q quits"
+  (assoc state :close true))
 
-(defn snapshot [canvas state framecount]
-  "Save a png with the code and state in its metadata"
-  (c/save canvas "tmp.png")
-  (metapng/bake "tmp.png" (str *ns* "-" framecount ".png") {
-    :code (slurp (str "src/" (replace (str *ns*) ["." "/"]) ".clj"))
-    :state (str state)
-    :author "Jonathan Dahan"}))
+(defmethod c/key-pressed [(str *ns*) c/virtual-key] [event state]
+  "print keypress"
+  (println (str "unknown key " event))
+  state)
 
 (defn prettify [state]
   "Only show human-understandable state - numbers, strings, booleans, round time"
@@ -89,20 +77,29 @@
         snapshot? (:snapshot window-state)
         close? (:close window-state)
         now (/ framecount (:fps window))
+        padding (* 2 (canvas-font-height canvas))
         w (c/width canvas)
         h (c/height canvas)
         scaled-width (* 0.8 w)
         scaled-height (* 0.9 h)
-        state (assoc window-state :time now :w w :h h :snapshot false)
+        state (assoc window-state :time now :w (c/width window) :h (c/height window))
         background-color (color/from-HSB (color/make-color (:hue state) 255 (:brightness state)))]
+    ; process events
     (if close? (c/close-window window))
     (if snapshot? (snapshot canvas state framecount))
-
+    ; draw grid
     (c/set-background canvas background-color)
     (doseq [day (:days state)]
       (c/set-font-attributes canvas (:font-size state) (if (:bold day) :bold :regular))
-      (right-text canvas (:text day) (* scaled-width (:x day)) (* scaled-height (:y day))))
-    (draw-debug canvas state)
+      (c/text canvas (:text day) (* scaled-width (:x day)) (* scaled-height (:y day)) :right))
+    ;draw debug
+    (if (:debug state)
+      (let [right (- (c/width canvas) padding)
+            bottom (- (c/height canvas) padding)
+            lines ["'d' toggles debug" "'s' screenshots" "'q' quits"]]
+        (doseq [[i line] (map-indexed vector lines)]
+          (c/text canvas line (- right padding) (- bottom (* padding (- 3 i))) :right))
+        (c/text canvas (prettify state) padding padding)))
     state))
 
 (let [first-of-year (t/date-time (t/year (t/now)) 1 1)
@@ -110,9 +107,9 @@
       months (partition-by t/month days)
       months-count (count months)
       longest-month (apply max (map count months))
-      width (* 2 (.getWidth (.getScreenSize (Toolkit/getDefaultToolkit))))
+      width (* 2 (c/screen-width))
       height (m/floor (* months-count (/ width longest-month) ))
-      canvas (c/make-canvas width height :high "Menlo")
+      canvas (c/make-canvas width height :highest "Menlo")
       state {:time 0
              :hue 0
              :brightness 255
